eb957185c053fb41800507de8dab4d8423cb1c58
Aidar <achakiev2003@gmail.com>
2021-01-27 12:00:31 +0600

    start model

---
+++ news/base/BaseViewController.swift
@@ -0,0 +1,64 @@
+//
+//  BaseViewController.swift
+//  news
+//
+//  Created by Айдар Чакиев on 27/1/21.
+//
+
+import RappleProgressHUD
+import UIKit
+
+let BASE_URL = "https://newsapi.org/v2/top-headlines?apiKey=ba88909bbda0454aaa503a8fff5e1225&country=ru"
+
+protocol BaseView {
+    func showLoad() -> Void
+    func hideLoad() -> Void
+    func error(error: String) -> Void
+}
+
+class BaseViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func getVC(name:String) -> UIViewController
+    {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: name)
+        return vc
+    }
+    
+    func back()
+    {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: - showLoading
+    func showLoading()
+    {
+        let attributes = RappleActivityIndicatorView.attribute(style: RappleStyle.circle, tintColor: .white, screenBG: .black, progressBG: .gray, progressBarBG: .orange, progreeBarFill: .red, thickness: 4)
+        RappleActivityIndicatorView.startAnimating(attributes: attributes)
+    }
+    
+    // MARK: - hideLoading
+    func hideLoading()
+    {
+        RappleActivityIndicatorView.stopAnimation()
+    }
+    
+    
+    func error(error: String)
+    {
+        showDialogOkBtn(title: "", message: error, vc: self)
+    }
+    
+    func showDialogOkBtn(title: String, message: String, vc: UIViewController)
+    {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+        }))
+        vc.present(alert, animated: true, completion: nil)
+    }
+    
+}
---
+++ news/model/NewsMainModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewsMainModel.swift
+//  news
+//
+//  Created by Айдар Чакиев on 27/1/21.
+//
+
+import Foundation
